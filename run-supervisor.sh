ea0a51fa721e73df23a2952f8c96d8985a1e84b9
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-09-24 16:50:40 +1000

    Got the supervisor and worker scripts working!

--- run-supervisor.sh
+++ run-supervisor.sh
@@ -8,35 +8,14 @@ cd $HOME/LogicCloud
 if [ ! -f finished-$SIZE ]
 then
     ./count-all-nqueens-solutions supervisor $SIZE --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
+            --end-stats=all --log-end-stats --log-stats=all \
             --log-format='$time:$prio:$message' \
-            &>> supervisor.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_1.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_2.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_3.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_4.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_5.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_6.log &
-    ./count-all-nqueens-solutions worker localhost --port=$PORT \
-            --end-stats=all --log-end-stats --log-stats-all \
-            --log-format='$time:$prio:$message' \
-            &>> worker_7.log &
+            >> supervisor.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_1.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_2.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_3.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_4.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_5.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_6.log 2>&1 &
+    ./count-all-nqueens-solutions worker localhost $PORT >> worker_7.log 2>&1 &
 fi
--- run-worker.sh
+++ run-worker.sh
@@ -5,35 +5,11 @@ PORT=`cat port`
 
 cd $HOME/LogicCloud
 
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_1.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_2.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_3.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_4.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_5.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_6.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_7.log &
-./count-all-nqueens-solutions worker $HOST --port=$PORT \
-        --end-stats=all --log-end-stats --log-stats-all \
-        --log-format='$time:$prio:$message' \
-        &>> worker_8.log &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_1.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_2.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_3.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_4.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_5.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_6.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_7.log 2>&1 &
+./count-all-nqueens-solutions worker $HOST $PORT >> worker_8.log 2>&1 &

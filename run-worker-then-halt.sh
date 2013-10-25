ad3ebe235e366506e6be54ec52d5c05b6b9efac9
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-10-25 02:30:16 +0000

    Split into a script that halts and one that doesn't; use args instead of files.

--- run-worker-then-halt.sh
+++ run-worker-then-halt.sh
@@ -2,7 +2,7 @@
 
 cd $HOME/Projects/LogicCloud
 
-./count-all-nqueens-solutions worker $1 $2 >> worker_1.log 2>&1 &
+(./count-all-nqueens-solutions worker $1 $2 >> worker_1.log 2>&1; /sbin/halt +1) &
 ./count-all-nqueens-solutions worker $1 $2 >> worker_2.log 2>&1 &
 ./count-all-nqueens-solutions worker $1 $2 >> worker_3.log 2>&1 &
 ./count-all-nqueens-solutions worker $1 $2 >> worker_4.log 2>&1 &
